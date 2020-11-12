d0abdcd92149d44b2093d9645e7d00494908622b
garynks <51041584+garynks@users.noreply.github.com>
2020-11-12 12:14:01 -0700

    Refactoring TroopManagement

--- attack.hpp
+++ attack.hpp
@@ -11,7 +11,7 @@ void attackUpgradeCompleted(UpgradeID uid);
 
 void train_unit(const Unit *trainer_unit, ABILITY_ID unit_to_train);
 
-int CountUnitType(const ObservationInterface* observation, UnitTypeID unit_type);
+static int CountUnitType(const ObservationInterface* observation, UnitTypeID unit_type);
 
 bool buildStructure(ABILITY_ID ability_type_for_structure, UNIT_TYPEID unit_type = UNIT_TYPEID::TERRAN_SCV);
 
