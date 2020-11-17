5dd79f43bc803e64f4681b6681d3532ede598be9
garynks <51041584+garynks@users.noreply.github.com>
2020-11-16 21:23:21 -0700

    Should build add on automatically once building is completed

--- TroopManager.hpp
+++ TroopManager.hpp
@@ -1,5 +1,5 @@
-#ifndef TROOPTRAINER_HPP
-#define TROOPTRAINER_HPP
+#ifndef TROOPMANAGER_HPP
+#define TROOPMANAGER_HPP
 
 //
 // Created by Gary Ng on 1st Nov
@@ -17,58 +17,68 @@
 
 using namespace sc2;
 
-class TroopManager {
+class TroopManager
+{
 public:
     TroopManager(threadsafe_priority_queue<Task> *t_queue, const ObservationInterface *obs)
-            : task_queue(t_queue), observation(obs) {
+        : task_queue(t_queue), observation(obs)
+    {
         enemy_locations = observation->GetGameInfo().enemy_start_locations; // positions of possible enemy starting locations
     }
 
-    void unitIdle(const Unit *unit) {
+    void unitIdle(const Unit *unit)
+    {
         // if unit is idle,
-        switch (unit->unit_type.ToType()) {
+        switch (unit->unit_type.ToType())
+        {
 
             // barracks train marine
             // TODO: possibly switch to Marauders if we already have a sufficient amount of Marines
             //
             // to train marauders, check for the presence of a tech lab first
 
-            case UNIT_TYPEID::TERRAN_BARRACKS: {
+        case UNIT_TYPEID::TERRAN_BARRACKS:
+        {
 
-                if (CountUnitType(observation, UNIT_TYPEID::TERRAN_MARINE) < 25) {
-                    task_queue->push(Task(TRAIN, ATTACK_AGENT, 5, ABILITY_ID::TRAIN_MARINE, UNIT_TYPEID::TERRAN_MARINE,
-                                          UNIT_TYPEID::TERRAN_BARRACKS, unit->tag));
-                }
-
-                task_queue->push(Task(TRAIN, ATTACK_AGENT, 5, ABILITY_ID::TRAIN_MARAUDER, UNIT_TYPEID::TERRAN_MARAUDER,
+            if (CountUnitType(observation, UNIT_TYPEID::TERRAN_MARINE) < 25)
+            {
+                task_queue->push(Task(TRAIN, ATTACK_AGENT, 5, ABILITY_ID::TRAIN_MARINE, UNIT_TYPEID::TERRAN_MARINE,
                                       UNIT_TYPEID::TERRAN_BARRACKS, unit->tag));
-                break;
             }
 
-            case UNIT_TYPEID::TERRAN_STARPORT: {
-                task_queue->push(Task(TRAIN, ATTACK_AGENT, 5, ABILITY_ID::TRAIN_BANSHEE, UNIT_TYPEID::TERRAN_BANSHEE,
-                                      UNIT_TYPEID::TERRAN_STARPORT, unit->tag));
-                task_queue->push(Task(TRAIN, ATTACK_AGENT, 5, ABILITY_ID::TRAIN_MEDIVAC, UNIT_TYPEID::TERRAN_MEDIVAC,
-                                      UNIT_TYPEID::TERRAN_STARPORT, unit->tag));
-                break;
-            }
+            task_queue->push(Task(TRAIN, ATTACK_AGENT, 5, ABILITY_ID::TRAIN_MARAUDER, UNIT_TYPEID::TERRAN_MARAUDER,
+                                  UNIT_TYPEID::TERRAN_BARRACKS, unit->tag));
+            break;
+        }
 
-            case UNIT_TYPEID::TERRAN_MARINE:
-            case UNIT_TYPEID::TERRAN_MEDIVAC: {
-                // TODO: Problems with the implementation; One unit for one location
-                task_queue->push(
-                        Task(ATTACK, ATTACK_AGENT, 7, unit, ABILITY_ID::ATTACK_ATTACK, enemy_locations.back()));
-                enemy_locations.pop_back();
-                break;
-            }
+        case UNIT_TYPEID::TERRAN_STARPORT:
+        {
+            task_queue->push(Task(TRAIN, ATTACK_AGENT, 5, ABILITY_ID::TRAIN_BANSHEE, UNIT_TYPEID::TERRAN_BANSHEE,
+                                  UNIT_TYPEID::TERRAN_STARPORT, unit->tag));
+            task_queue->push(Task(TRAIN, ATTACK_AGENT, 5, ABILITY_ID::TRAIN_MEDIVAC, UNIT_TYPEID::TERRAN_MEDIVAC,
+                                  UNIT_TYPEID::TERRAN_STARPORT, unit->tag));
+            break;
+        }
+
+        case UNIT_TYPEID::TERRAN_MARINE:
+        case UNIT_TYPEID::TERRAN_MEDIVAC:
+        {
+            // TODO: Problems with the implementation; One unit for one location
+            //                task_queue->push(
+            //                        Task(ATTACK, ATTACK_AGENT, 7, unit, ABILITY_ID::ATTACK_ATTACK, enemy_locations.back()));
+            //                enemy_locations.pop_back();
+            break;
+        }
         }
     }
 
     // helps count the number of units present in the current game state
-    static int CountUnitType(const ObservationInterface *obs, UnitTypeID unit_type) {
+    static int CountUnitType(const ObservationInterface *obs, UnitTypeID unit_type)
+    {
         int count = 0;
         Units my_units = obs->GetUnits(Unit::Alliance::Self);
-        for (const auto unit : my_units) {
+        for (const auto unit : my_units)
+        {
             if (unit->unit_type == unit_type)
                 ++count;
         }
