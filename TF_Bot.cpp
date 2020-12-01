5b91282a53e7605996fe786c833da18637879754
Waseem Yusuf <waseem@showbie.com>
2020-11-30 17:58:19 -0700

    Add multithreading

--- TF_Bot.cpp
+++ TF_Bot.cpp
@@ -39,50 +39,123 @@ void TF_Bot::OnGameEnd() {
 }
 
 void TF_Bot::OnStep() {
-    resource->step();
-    attack->step();
-    defence->step();
-    scout->step();
+    std::thread resource_thread([this]() { resource->step(); });
+    std::thread attack_thread([this]() { attack->step(); });
+    std::thread defence_thread([this]() { defence->step(); });
+    std::thread scout_thread([this]() { scout->step(); });
+
+    resource_thread.join();
+    defence_thread.join();
+    attack_thread.join();
+    scout_thread.join();
+
+
+    // resource->step();
+    // attack->step();
+    // defence->step();
+    // scout->step();
 }
 
 void TF_Bot::OnUnitDestroyed(const Unit* unit) {
-    resource->unitDestroyed(unit);
-    defence->unitDestroyed(unit);
-    attack->unitDestroyed(unit);
-    scout->unitDestroyed(unit);
+    std::thread resource_thread([this, unit]() { resource->unitDestroyed(unit); });
+    std::thread attack_thread([this, unit]() { attack->unitDestroyed(unit); });
+    std::thread defence_thread([this, unit]() { defence->unitDestroyed(unit); });
+    std::thread scout_thread([this, unit]() { scout->unitDestroyed(unit); });
+
+    resource_thread.join();
+    defence_thread.join();
+    attack_thread.join();
+    scout_thread.join();
+
+    // resource->unitDestroyed(unit);
+    // defence->unitDestroyed(unit);
+    // attack->unitDestroyed(unit);
+    // scout->unitDestroyed(unit);
 }
 
 void TF_Bot::OnUnitCreated(const Unit* unit) {
-    resource->unitCreated(unit); // resource will call addUnit() to the appropriate agent
-    attack->unitCreated(unit); // this is where most of the logic for when to build what is
-    defence->unitCreated(unit);
-    scout->unitCreated(unit);
+    // resource will call addUnit() to the appropriate agent 
+    std::thread resource_thread([this, unit]() { resource->unitCreated(unit); });
+    // this is where most of the logic for when to build what is
+    std::thread attack_thread([this, unit]() { attack->unitCreated(unit); });
+    std::thread defence_thread([this, unit]() { defence->unitCreated(unit); });
+    std::thread scout_thread([this, unit]() { scout->unitCreated(unit); });
+
+    resource_thread.join();
+    defence_thread.join();
+    attack_thread.join();
+    scout_thread.join();
+
+    // resource->unitCreated(unit); // resource will call addUnit() to the appropriate agent
+    // attack->unitCreated(unit); // this is where most of the logic for when to build what is
+    // defence->unitCreated(unit);
+    // scout->unitCreated(unit);
 }
 
 void TF_Bot::OnUnitIdle(const Unit* unit) {
-    resource->unitIdle(unit);
-    attack->unitIdle(unit);
-    defence->unitIdle(unit);
-    scout->unitIdle(unit);
+    std::thread resource_thread([this, unit]() { resource->unitIdle(unit); });
+    std::thread attack_thread([this, unit]() { attack->unitIdle(unit); });
+    std::thread defence_thread([this, unit]() { defence->unitIdle(unit); });
+    std::thread scout_thread([this, unit]() { scout->unitIdle(unit); });
+
+    resource_thread.join();
+    defence_thread.join();
+    attack_thread.join();
+    scout_thread.join();
+
+    // resource->unitIdle(unit);
+    // attack->unitIdle(unit);
+    // defence->unitIdle(unit);
+    // scout->unitIdle(unit);
 }
 
 void TF_Bot::OnUpgradeCompleted(UpgradeID uid) {
-    resource->upgradeCompleted(uid);
-    attack->upgradeCompleted(uid);
-    defence->upgradeCompleted(uid);
-    scout->upgradeCompleted(uid);
+    std::thread resource_thread([this, uid]() { resource->upgradeCompleted(uid); });
+    std::thread attack_thread([this, uid]() { attack->upgradeCompleted(uid); });
+    std::thread defence_thread([this, uid]() { defence->upgradeCompleted(uid); });
+    std::thread scout_thread([this, uid]() { scout->upgradeCompleted(uid); });
+
+    resource_thread.join();
+    defence_thread.join();
+    attack_thread.join();
+    scout_thread.join();
+
+    // resource->upgradeCompleted(uid);
+    // attack->upgradeCompleted(uid);
+    // defence->upgradeCompleted(uid);
+    // scout->upgradeCompleted(uid);
 }
 
 void TF_Bot::OnBuildingConstructionComplete(const Unit* unit) {
-    resource->buildingConstructionComplete(unit);
-    attack->buildingConstructionComplete(unit);
-    defence->buildingConstructionComplete(unit);
-    scout->buildingConstructionComplete(unit);
+    std::thread resource_thread([this, unit]() { resource->buildingConstructionComplete(unit); });
+    std::thread attack_thread([this, unit]() { attack->buildingConstructionComplete(unit); });
+    std::thread defence_thread([this, unit]() { defence->buildingConstructionComplete(unit); });
+    std::thread scout_thread([this, unit]() { scout->buildingConstructionComplete(unit); });
+
+    resource_thread.join();
+    defence_thread.join();
+    attack_thread.join();
+    scout_thread.join();
+
+    // resource->buildingConstructionComplete(unit);
+    // attack->buildingConstructionComplete(unit);
+    // defence->buildingConstructionComplete(unit);
+    // scout->buildingConstructionComplete(unit);
 }
 
 void TF_Bot::OnUnitEnterVision(const Unit* unit) {
-    resource->unitEnterVision(unit);
-    defence->unitEnterVision(unit);
-    attack->unitEnterVision(unit);
-    scout->unitEnterVision(unit);
+    std::thread resource_thread([this, unit]() { resource->unitEnterVision(unit); });
+    std::thread attack_thread([this, unit]() { attack->unitEnterVision(unit); });
+    std::thread defence_thread([this, unit]() { defence->unitEnterVision(unit); });
+    std::thread scout_thread([this, unit]() { scout->unitEnterVision(unit); });
+
+    resource_thread.join();
+    defence_thread.join();
+    attack_thread.join();
+    scout_thread.join();
+
+    // resource->unitEnterVision(unit);
+    // defence->unitEnterVision(unit);
+    // attack->unitEnterVision(unit);
+    // scout->unitEnterVision(unit);
 }
